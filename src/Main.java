85cc5b4b332fd18f84aa563547314a029010ac85
Ekaterina <Katerina.rekiyan@yandex.ru>
2022-10-11 10:13:28 +0300

    add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int milePrice = 20;
+        int miles = ticketPrice / milePrice;
+        System.out.println(miles);
+
+    }
+}
